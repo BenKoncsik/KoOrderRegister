25ec2af9970498fcde982ffec3afa53c09c436ab
Benedek Koncsik <koncsik.benedek.andras@gmail.com>
2024-09-05 12:08:33 +0200

    #5 Add Email validátor

---
+++ KoOrderRegister/EntryChekers/EmailValidationBehavior.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoOrderRegister.EntryCheckers
+{
+    public class EmailValidationBehavior : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            bindable.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(bindable);
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            bindable.TextChanged -= OnEntryTextChanged;
+            base.OnDetachingFrom(bindable);
+        }
+
+        private void OnEntryTextChanged(object sender, TextChangedEventArgs args)
+        {
+            if (sender is Entry entry)
+            {
+                if (IsValidEmail(args.NewTextValue))
+                {
+                    entry.TextColor = Application.Current.UserAppTheme == AppTheme.Dark ? Colors.Black : Colors.White;
+                }
+                else
+                {
+                    entry.TextColor = Colors.Red; 
+                }
+            }
+        }
+
+        bool IsValidEmail(string email)
+        {
+            try
+            {
+                var mail = new MailAddress(email);
+                return mail.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
