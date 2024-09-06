b0b120b364c94ef245d729b7ca91e9729340dfee
Benedek Koncsik <koncsik.benedek.andras@gmail.com>
2024-09-06 11:24:12 +0200

    add language

---
+++ KoOrderRegister/Localization/SupportedLanguage/ILanguageSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoOrderRegister.Localization.SupportedLanguage
+{
+    public interface ILanguageSettings
+    {
+        Task<string> GetLanguageName();
+        Task<string> GetLanguageDisplayName();
+        Task<long> GetLanguageId();
+        Task SetRegioSpecification();
+
+
+    }
+}
