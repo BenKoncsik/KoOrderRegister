2e924674b52645d3ba955f62500b23888a7ef10a
Benedek Koncsik <koncsik.benedek.andras@gmail.com>
2024-09-25 12:39:48 +0200

    #92 update

--- build.bat
+++ build.bat
@@ -13,28 +13,20 @@ if not exist "KoOrderRegister\KoOrderRegister.csproj" (
 )
 
 set "CS_PROJECT=KoOrderRegister\KoOrderRegister.csproj"
-set "OUTPUT_DIR_BUILD=output\build"
-set "OUTPUT_DIR=output"
+set "OUTPUT_DIR_BUILD=\output\build"
+set "OUTPUT_DIR=\output"
 
 echo KEYPASS from environment: %KEYPASS%
-set "OUTPUT_DIR=/output"
 
 echo Reading current version from version.txt...
-set /p CURRENT_VERSION=<version.txt
-echo Current version is %CURRENT_VERSION%
 
-REM Verziószám kezelése
-for /f "tokens=1-3 delims=." %%a in ("%CURRENT_VERSION%") do (
-    set "major=%%a"
-    set "minor=%%b"
-    set "build=%%c"
-)
+echo Version is %NEW_VERSION%
+echo Version code is %NEW_VERSION_CODE%
 
-set "NEW_VERSION=%major%.%minor%.%build%"
-echo Updated version to %NEW_VERSION%
+
 
 echo Publishing the application...
-dotnet publish "%CS_PROJECT%" -f net8.0-android -c Release -p:AndroidKeyStore=true -p:AndroidSigningKeyStore=kor.keystore -p:AndroidSigningKeyAlias=kor_pub -p:AndroidSigningKeyPass=%KEYPASS% -p:AndroidSigningStorePass=%KEYPASS% -p:AndroidVersionCode=%major% -p:AndroidVersionName=%NEW_VERSION%  -o "%OUTPUT_DIR_BUILD%"
+dotnet publish "%CS_PROJECT%" -f net8.0-android -c Release -p:AndroidKeyStore=true -p:AndroidSigningKeyStore=kor.keystore -p:AndroidSigningKeyAlias=kor_pub -p:AndroidSigningKeyPass=%KEYPASS% -p:AndroidSigningStorePass=%KEYPASS% -p:AndroidVersionCode=%NEW_VERSION_CODE% -p:AndroidVersionName=%NEW_VERSION%  -o "%OUTPUT_DIR_BUILD%"
 
 set "ORIGINAL_APK=%OUTPUT_DIR_BUILD%\hu.kncsk.koorderregister-Signed.apk"
 set "NEW_APK_NAME=%OUTPUT_DIR%\KoOrderRegister_%NEW_VERSION%_android.apk"
