621ef609b5eb4575b1edc3e12d1c4f7a5e03720f
Benedek Koncsik <koncsik.benedek.andras@gmail.com>
2024-09-25 18:55:54 +0200

    #92

--- build.bat
+++ build.bat
@@ -8,9 +8,28 @@ if not exist "KoOrderRegister\KoOrderRegister.csproj" (
 
 set "CS_PROJECT=KoOrderRegister\KoOrderRegister.csproj"
 set "OUTPUT_DIR_BUILD=output\build"
-REM set "OUTPUT_DIR_BUILD=bin\Release\net8.0-android"
 set "OUTPUT_DIR=output"
-REM set "OUTPUT_DIR=bin\Release"
+
+
+echo Reading current version...
+for /f "tokens=3 delims=<>" %%a in ('findstr "ApplicationDisplayVersion" %CS_PROJECT%') do set "CURRENT_VERSION=%%a"
+echo Current version is %CURRENT_VERSION%
+
+REM Bontsa szét a verziót pontok mentén
+for /f "tokens=1-3 delims=." %%a in ("%CURRENT_VERSION%") do (
+    set "major=%%a"
+    set "minor=%%b"
+    set "build=%%c"
+    REM set "revision=%%d"
+)
+
+REM Növelje a harmadik verziószámot
+set /a "build+=1"
+
+echo Updated version to %NEW_VERSION%
+
+echo Updating the project file with new version...
+powershell -Command "(gc '%CS_PROJECT%') -replace '<ApplicationDisplayVersion>%NEW_VERSION%</ApplicationDisplayVersion>', '<ApplicationDisplayVersion>%NEW_VERSION%</ApplicationDisplayVersion>' | Out-File -encoding UTF8 '%CS_PROJECT%'"
 
 echo Create dictonary %OUTPUT_DIR_BUILD%
 mkdir %OUTPUT_DIR_BUILD%
