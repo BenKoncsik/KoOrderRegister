dcd319c60b41dec816c4ce4c756f905fb72278d8
Benedek Koncsik <koncsik.benedek.andras@gmail.com>
2024-10-01 10:41:58 +0200

    #92 update windows version

--- build.bat
+++ build.bat
@@ -7,6 +7,7 @@ if not exist "KoOrderRegister\KoOrderRegister.csproj" (
 )
 
 set "CS_PROJECT=KoOrderRegister\KoOrderRegister.csproj"
+set "APPX_MANIFEST=Platforms/windows/Package.appxmanifest"
 set "OUTPUT_DIR_BUILD=output\build"
 set "OUTPUT_DIR=output"
 
@@ -14,6 +15,7 @@ set "OUTPUT_DIR=output"
 echo Reading current version...
 for /f "tokens=3 delims=<>" %%a in ('findstr "ApplicationDisplayVersion" %CS_PROJECT%') do set "CURRENT_VERSION=%%a"
 echo Current version is %CURRENT_VERSION%
+set "WINDOWS_CURRENT_VERSION=%CURRENT_VERSION%.0"
 
 REM Bontsa szét a verziót pontok mentén
 for /f "tokens=1-3 delims=." %%a in ("%CURRENT_VERSION%") do (
@@ -25,8 +27,17 @@ for /f "tokens=1-3 delims=." %%a in ("%CURRENT_VERSION%") do (
 
 echo Updated version to %NEW_VERSION%
 
+set "WINDOWS_NEW_VERSION=%NEW_VERSION%.0"
+echo Windows version to: %WINDOWS_NEW_VERSION%
+
 echo Updating the project file with new version...
+REM android
 powershell -Command "(gc '%CS_PROJECT%') -replace '<ApplicationDisplayVersion>%CURRENT_VERSION%</ApplicationDisplayVersion>', '<ApplicationDisplayVersion>%NEW_VERSION%</ApplicationDisplayVersion>' | Out-File -encoding UTF8 '%CS_PROJECT%'"
+
+REM windows
+powershell -Command "(gc '%APPX_MANIFEST%') -replace '<Identity Name="maui-package-name-placeholder" Publisher="CN=koncs" Version="%WINDOWS_CURRENT_VERSION%" />', '<ApplicationDisplayVersion>%WINDOWS_NEW_VERSION%</ApplicationDisplayVersion>' | Out-File -encoding UTF8 '%APPX_MANIFEST%'"
+
+
 
 echo Create dictonary %OUTPUT_DIR_BUILD%
 mkdir %OUTPUT_DIR_BUILD%
@@ -56,8 +67,7 @@ if exist "%ORIGINAL_APK%" (
 
 echo Build and rename process completed. New APK: %NEW_APK_NAME%
 
-set "WINDOWS_NEW_VERSION=%NEW_VERSION%.0"
-echo Windows version to: %WINDOWS_NEW_VERSION%
+
 
 
 echo Building MSIX package for Windows x64...
@@ -66,7 +76,20 @@ dotnet publish ".\KoOrderRegister\KoOrderRegister.csproj" -r win-x64 -c Release 
 
 REM Másolja az MSIX csomagot az általános kimeneti könyvtárba
 echo Copying MSIX package to general output directory...
-xcopy "%OUTPUT_DIR%\KoOrderRegister_%NEW_VERSION%_Test\*.msix" "%OUTPUT_DIR%" /Y /I
+$msixFile = Get-ChildItem -Path $OUTPUT_DIR -Filter "*.msix" | Select-Object -First 1
+
+if ($msixFile) {
+    # Új fájlnév létrehozása
+    $newFileName = "KoOrderRegister_$NEW_VERSION_X64.msix"
+    $newFilePath = Join-Path $OUTPUT_DIR $newFileName
+
+    # Fájl átnevezése
+    Rename-Item -Path $msixFile.FullName -NewName $newFileName
+    Write-Host "File renamed: $newFilePath"
+} else {
+    Write-Host "No msix file $OUTPUT_DIR"
+}
+xcopy "%OUTPUT_DIR%\KoOrderRegister_%NEW_VERSION%_X64\*.msix" "%OUTPUT_DIR%" /Y /I
 echo Copy completed.
 echo MSIX build process completed.
 
