463bff34bf8488d269c338720bce30da52c35cac
Koncsik Benedek <75886141+BenKoncsik@users.noreply.github.com>
2024-10-08 21:29:48 +0200

    77 export data (#186)
    * #77 export is done hyper link not work yet
    
    * #172 remove windows using

--- KoOrderRegister/Modules/Export/Excel/ExcelExportService.cs
+++ KoOrderRegister/Modules/Export/Excel/ExcelExportService.cs
@@ -9,8 +9,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Windows.Gaming.Input.Custom;
-using Windows.Storage;
 
 namespace KoOrderRegister.Modules.Export.Excel
 {
