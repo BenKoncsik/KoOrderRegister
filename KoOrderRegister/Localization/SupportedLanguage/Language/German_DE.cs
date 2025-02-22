60a6e7ad75dd99f68a92f142e5a2a6547a035b0d
Koncsik Benedek <75886141+BenKoncsik@users.noreply.github.com>
2025-02-22 10:00:32 +0100

    244 add russian language (#255)
    * #244 add russion language and machin translator
    
    * #244 add rusian language, translator, workflows
    
    #244 add rusian language and add machin translator python script and update workflows

--- KoOrderRegister/Localization/SupportedLanguage/Language/German_DE.cs
+++ KoOrderRegister/Localization/SupportedLanguage/Language/German_DE.cs
@@ -12,7 +12,7 @@ namespace KoOrderRegister.Localization.SupportedLanguage.Language
     {
         private string _languageName = "German_DE";
         private string _cultureName = "de-DE";
-        private string _languageDisplayName = "Deutsch (German) [AI Translated]";
+        private string _languageDisplayName = "Deutsch (German)";
         private string _languageId = "+49";
 
         public string DisplayName { get => GetLanguageDisplayName(); }
